86952c0071dd5aa1d210ed35fe2ae7d12a0368ff
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2019-02-09 09:58:15 -0800

    last commit before deploying

--- assets/javascript/sources.js
+++ assets/javascript/sources.js
@@ -109,7 +109,6 @@ $(document).ready(function () {
     
 });
 
-<!-- Cards -->
 
 
 
@@ -118,15 +117,7 @@ $(document).ready(function () {
 
 
 
-
-
-});
-
-
-
-
-
-
+// });
 
 
 
@@ -139,125 +130,117 @@ $(document).ready(function () {
 
 
 
-    // function initMap() {
+
+
+
+
+
+
+//     // function initMap() {
         
-    //     var map = new google.maps.Map(document.getElementById('map'), {
-    //       zoom: 8,
-    //       center: {lat: -34.397, lng: 150.644}
-    //     });
-    //     var geocoder = new google.maps.Geocoder();
+//     //     var map = new google.maps.Map(document.getElementById('map'), {
+//     //       zoom: 8,
+//     //       center: {lat: -34.397, lng: 150.644}
+//     //     });
+//     //     var geocoder = new google.maps.Geocoder();
 
-    //     document.getElementById('submit').addEventListener('click', function() {
-    //       geocodeAddress(geocoder, map);
-    //     });
-    //   }
+//     //     document.getElementById('submit').addEventListener('click', function() {
+//     //       geocodeAddress(geocoder, map);
+//     //     });
+//     //   }
 
-    //   function geocodeAddress(geocoder, resultsMap) {
-    //     var address = document.getElementById('address').value;
-    //     geocoder.geocode({'address': address}, function(results, status) {
-    //       if (status === 'OK') {
-    //         resultsMap.setCenter(results[0].geometry.location);
-    //         var marker = new google.maps.Marker({
-    //           map: resultsMap,
-    //           position: results[0].geometry.location
-    //         });
-    //       } else {
-    //         alert('Geocode was not successful for the following reason: ' + status);
-    //       }
-    //     });
-    //   }
-    //    ------> Hiking Project API 
+//     //   function geocodeAddress(geocoder, resultsMap) {
+//     //     var address = document.getElementById('address').value;
+//     //     geocoder.geocode({'address': address}, function(results, status) {
+//     //       if (status === 'OK') {
+//     //         resultsMap.setCenter(results[0].geometry.location);
+//     //         var marker = new google.maps.Marker({
+//     //           map: resultsMap,
+//     //           position: results[0].geometry.location
+//     //         });
+//     //       } else {
+//     //         alert('Geocode was not successful for the following reason: ' + status);
+//     //       }
+//     //     });
+//     //   }
+//     //    ------> Hiking Project API 
 
-        // //sidenav
-        // const sideNav = document.querySelector('.sidenav');
-        // M.Sidenav.init(sideNav, {});
+//         // //sidenav
+//         // const sideNav = document.querySelector('.sidenav');
+//         // M.Sidenav.init(sideNav, {});
 
-        // $('.slider').slider();
+//         // $('.slider').slider();
 
-        // // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
-        // var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + latUser + "&lon=" + lngUser + "&maxDistance=15&key=200415127-68adde2ff6be3226f8cb65a7535b3ecc";
+//         // // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
+//         // var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + latUser + "&lon=" + lngUser + "&maxDistance=15&key=200415127-68adde2ff6be3226f8cb65a7535b3ecc";
 
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // }).then(function (response) {
-        //     console.log(response.trails);
-        //     for (var i = 0; i < response.trails.length; i++) {
-        //         $(".test").append(
+//         // $.ajax({
+//         //     url: queryURL,
+//         //     method: "GET"
+//         // }).then(function (response) {
+//         //     console.log(response.trails);
+//         //     for (var i = 0; i < response.trails.length; i++) {
+//         //         $(".test").append(
 
-        //             "<div class='col s12 m4'>" +
-        //             "<div class='card large'>" +
-        //             "<div class='card-image'>" +
-        //             "<img class='generatedImgs' src=" + response.trails[i].imgSmallMed + ">" +
-        //             "<span class='card-title'>" + response.trails[i].name + "</span>" +
-        //             "</div>" +
-        //             "<div class='card-content'>" + response.trails[i].location +
-        //             "<br>Summary: " + response.trails[i].summary +
-        //             "<br>Length: " + response.trails[i].length + " miles" +
-        //             "<br>Ascent: " + response.trails[i].high + " ft" +
-        //             "<br>Condition: " + response.trails[i].conditionStatus + "</div>" +
-        //             "<div class='card-action'>" +
-        //             "<a class='waves-effect waves-light btn-large' target='_blank' href=" + response.trails[i].url + ">Details at Hiking Project</a>" +
-        //             "</div>" +
-        //             "</div>" +
-        //             "</div>")
-        //     }
-        // });
+//         //             "<div class='col s12 m4'>" +
+//         //             "<div class='card large'>" +
+//         //             "<div class='card-image'>" +
+//         //             "<img class='generatedImgs' src=" + response.trails[i].imgSmallMed + ">" +
+//         //             "<span class='card-title'>" + response.trails[i].name + "</span>" +
+//         //             "</div>" +
+//         //             "<div class='card-content'>" + response.trails[i].location +
+//         //             "<br>Summary: " + response.trails[i].summary +
+//         //             "<br>Length: " + response.trails[i].length + " miles" +
+//         //             "<br>Ascent: " + response.trails[i].high + " ft" +
+//         //             "<br>Condition: " + response.trails[i].conditionStatus + "</div>" +
+//         //             "<div class='card-action'>" +
+//         //             "<a class='waves-effect waves-light btn-large' target='_blank' href=" + response.trails[i].url + ">Details at Hiking Project</a>" +
+//         //             "</div>" +
+//         //             "</div>" +
+//         //             "</div>")
+//         //     }
+//         // });
 
         
 
 
-    // /*-------------------------------START  of   Google API  ------------------------------------
-    //             When user enters Zipcode, we are able to get their LAT & LONG coordinates
-    // ------------------------------------------------------------------------------------------- */
+//     // /*-------------------------------START  of   Google API  ------------------------------------
+//     //             When user enters Zipcode, we are able to get their LAT & LONG coordinates
+//     // ------------------------------------------------------------------------------------------- */
 
-    // function initMap(){
-    //     var map = new google.maps.Map(document.getElementById('map'), {
-    //         zoom: 8,
-    //         center: { lat: -34.397, lng: 150.644 }
-    //     });
-    //     var geocoder = new google.maps.Geocoder();
+//     // function initMap(){
+//     //     var map = new google.maps.Map(document.getElementById('map'), {
+//     //         zoom: 8,
+//     //         center: { lat: -34.397, lng: 150.644 }
+//     //     });
+//     //     var geocoder = new google.maps.Geocoder();
 
-    //     ocument.getElementById('submitBtn').addEventListener('click', function () {
-    //     geocodeAddress(geocoder, map);
-    //     });
-    // }      // End of initMap()
+//     //     ocument.getElementById('submitBtn').addEventListener('click', function () {
+//     //     geocodeAddress(geocoder, map);
+//     //     });
+//     // }      // End of initMap()
 
-    // latUser = '';
-    // lngUser = '';
-    // function geocodeAddress(geocoder, resultsMap) {
-    //     var userZip = document.getElementById('userZip').value;
-    //     geocoder.geocode({ 'userZip': userZip }, function (results, status) {
-    //         if (status === 'OK') {
-    //             resultsMap.setCenter(results[0].geometry.location);
-    //             // alert("1-- " + results[0].geometry.location); 
-    //             var latUser = results[0].geometry.location.lat();
-    //             var lngUser = results[0].geometry.location.lng();
-    //             alert(latUser);
-    //             alert("LONG: " + lngUser);
+//     // latUser = '';
+//     // lngUser = '';
+//     // function geocodeAddress(geocoder, resultsMap) {
+//     //     var userZip = document.getElementById('userZip').value;
+//     //     geocoder.geocode({ 'userZip': userZip }, function (results, status) {
+//     //         if (status === 'OK') {
+//     //             resultsMap.setCenter(results[0].geometry.location);
+//     //             // alert("1-- " + results[0].geometry.location); 
+//     //             var latUser = results[0].geometry.location.lat();
+//     //             var lngUser = results[0].geometry.location.lng();
+//     //             alert(latUser);
+//     //             alert("LONG: " + lngUser);
 
-    //             var marker = new google.maps.Marker({
-    //                 map: resultsMap,
-    //                 position: results[0].geometry.location
-    //             });
-    //         } else {
-    //             alert('Geocode was not successful for the following reason: ' + status);
-    //         }
-    //     });
-    // }
+//     //             var marker = new google.maps.Marker({
+//     //                 map: resultsMap,
+//     //                 position: results[0].geometry.location
+//     //             });
+//     //         } else {
+//     //             alert('Geocode was not successful for the following reason: ' + status);
+//     //         }
+//     //     });
+//     // }
         
-
-
-
-
-
-
-    
-
-
-
-        
-
-
-
 
